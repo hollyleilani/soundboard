73cf0e6d0a1d48e9ea0e0d4e727d5b96060960ee
Holly <145307009+hollyleilani@users.noreply.github.com>
2024-01-22 00:35:32 +0000

    1/21/24

--- lib/ui/lib/ui/new_recording.dart
+++ lib/ui/lib/ui/new_recording.dart
@@ -1,4 +1,17 @@
-class NewRecording extends StatelessWidget {
+import 'package:flutter/material.dart';
+
+
+class NewRecording extends  StatefulWidget {
+  const NewRecording({super.key});
+
+  @override
+  State<NewRecording> createState() => NewRecordingState();
+}
+
+class NewRecordingState extends State<NewRecording> {
+  bool isRecording = false;
+  String? audio;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -6,7 +19,49 @@ class NewRecording extends StatelessWidget {
         title: Text('New Recording'),
       ),
       body: Center(
-        child: Text('Welcome to the new screen!'),
+        child: Column(
+          children: [
+            TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: 'Title',
+              ),
+            ),
+            !isRecording ? ElevatedButton(
+              onPressed: () {
+                setState(() {
+                  isRecording = true;
+                });
+              },
+              child: const Text('Record'),
+            ) : SizedBox(),
+            isRecording ? ElevatedButton (
+              onPressed: () {
+                setState(() {
+                  isRecording = false;
+                });
+              },
+              child: const Text('Stop'),
+            ),
+             isRecording ? ElevatedButton (
+              onPressed: () {
+                setState((){
+                  isRecording = false;
+                });
+              },
+              child: const Text('Play'),
+            ),
+             isRecording ? ElevatedButton (
+              onPressed: () {
+                setState(() {
+                  isRecording = false;
+                });
+              },
+              child: const Text('Save'),
+            ),
+          ],
+        ),
+
       ),
     );
   }
