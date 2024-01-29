bf313650c281433571eb2317266a8a75fba0d56b
Holly <145307009+hollyleilani@users.noreply.github.com>
2024-01-29 00:03:32 +0000

    a

--- lib/ui/lib/ui/new_recording.dart
+++ lib/ui/lib/ui/new_recording.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:record/record.dart';
 
 
 class NewRecording extends  StatefulWidget {
@@ -11,6 +12,7 @@ class NewRecording extends  StatefulWidget {
 class NewRecordingState extends State<NewRecording> {
   bool isRecording = false;
   String? audio;
+  final record = AudioRecorder();
 
   @override
   Widget build(BuildContext context) {
@@ -28,7 +30,10 @@ class NewRecordingState extends State<NewRecording> {
               ),
             ),
             !isRecording ? ElevatedButton(
-              onPressed: () {
+              onPressed: () async {
+                print("record me");
+                await record.start(const RecordConfig(encoder: AudioEncoder.wav), path: 'aFullPath/myFile.m4a');
+                // todo record audio here
                 setState(() {
                   isRecording = true;
                 });
@@ -36,21 +41,23 @@ class NewRecordingState extends State<NewRecording> {
               child: const Text('Record'),
             ) : SizedBox(),
             isRecording ? ElevatedButton (
-              onPressed: () {
+              onPressed: () async {
+                final path = await record.stop();
+
                 setState(() {
                   isRecording = false;
                 });
               },
               child: const Text('Stop'),
-            ),
+            ) : SizedBox(),
              isRecording ? ElevatedButton (
-              onPressed: () {
+              onPressed: () async {
                 setState((){
                   isRecording = false;
                 });
               },
               child: const Text('Play'),
-            ),
+            ): SizedBox(),
              isRecording ? ElevatedButton (
               onPressed: () {
                 setState(() {
@@ -58,7 +65,7 @@ class NewRecordingState extends State<NewRecording> {
                 });
               },
               child: const Text('Save'),
-            ),
+            ): SizedBox(),
           ],
         ),
 
